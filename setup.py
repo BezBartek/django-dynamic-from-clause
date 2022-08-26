b294804ca5c5cc4e855a77246bc4fde1cbe8c697
bartlomiej <n.bartek3762@gmail.com>
2022-08-26 10:40:31 +0000

    Version 0.0.6

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="django-dynamic-from-clause",
-    version="0.0.5",
+    version="0.0.6",
     description="Gives the ability to dynamically configure SQL For clause for models. "
                 "This give you ability to wrap any sql into models and use ORM features on it.",
     keywords=[
